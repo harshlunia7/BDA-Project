bcd62c898c127067d7c1707f0a89cb52a2bb50a6
Anish <anish@Anishs-MacBook-Pro.local>
2019-11-25 06:20:54 +0530

    entry removed after click

--- components/bdaRead2.py
+++ components/bdaRead2.py
@@ -6,7 +6,7 @@ mycol = mydb['suicideCollection']
 w = csv.writer(open('/Users/anish/Desktop/tkinter-gui-application-examples-master/components/Read.csv', 'w'))
 w.writerow(['_id',	'country','year','sex','age','suicides_no','population','suicides/100kpop','gdp_for_year($)','gdp_per_capita'])
 w = csv.writer(open('/Users/anish/Desktop/tkinter-gui-application-examples-master/components/Read.csv', 'a'))
-for x in mycol.find({'country':'Bangladesh','sex':'male'}):
+for x in mycol.find({'country':'Singapore','year':1998,'sex':'male','age':'21','suicides_no':'0','population':'0','suicides/100k pop':'0','gdp_for_year ($)':'0','gdp_per_capita ($)':'0'}):
 	a=[]
 	for key,val in x.items():
 		a.append(val)
--- components/bdaUpdate2.py
+++ components/bdaUpdate2.py
@@ -3,4 +3,4 @@ import csv
 myclient = pymongo.MongoClient('mongodb://localhost:27017/')
 mydb = myclient['suicideRate']
 mycol = mydb['suicideCollection']
-mycol.update({'sex':'male','country':'Bangladesh','year':1998,'age':'21'},{'$set':{'suicides_no':0,'population':0,'suicides/100k pop':'0','gdp_for_year ($)':'0','gdp_per_capita ($)':'11'}})
+mycol.update({'sex':'male','country':'Singapore','year':1998,'age':'21'},{'$set':{'suicides_no':0,'population':0,'suicides/100k pop':'0','gdp_for_year ($)':'0','gdp_per_capita ($)':'10'}})
